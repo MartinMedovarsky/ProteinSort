176bc46d2f79a77dc9e952b31a5c553cf7c86b4e
Martin Medovarsky <martinmedovarsky1@gmail.com>
2021-02-21 21:38:59 +1100

    Added a variety of checks for bad input

--- categoryRequests.py
+++ categoryRequests.py
@@ -7,7 +7,7 @@ import json
 #Clears current itemData file.
 with open("itemData.csv", "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["ID","name","description","imgURL","cat","subCat","price","packSize","cupPrice","cupMeasure","PPGP","PPP","PPS","veg","allergens"])
+        writer.writerow(["ID","name","description","imgURL","cat","subCat","price","packSize","cupPrice","cupMeasure","PPGP","PPS","veg","allergens"])
 
 #The ID codes used within API calls
 #Using categories instead of all to illiminate non-food categories
@@ -25,9 +25,9 @@ def pageCheck(itemCount):
 #Calcualates values needed for the database, including price per gram of protein
 def calculations(r_dict, x):
     PPGP = 0 #Price Per Gram of Protein
-    PPP = 0 #Protein Per Product
     PPS = 0 #Protein Per Serve
     pContent = 0 #Protein content per 100 grams of product
+    servings = "0" #Servings per package
 
     #Nutritional information string
     nutri = r_dict["Bundles"][x]["Products"][0]["AdditionalAttributes"]["nutritionalinformation"]
@@ -42,18 +42,26 @@ def calculations(r_dict, x):
     cupMeasure = r_dict["Bundles"][x]["Products"][0]["CupMeasure"]
     altWeight = r_dict["Bundles"][x]["Products"][0]["PackageSize"] #This weight is accurate for packaged products
 
-    #Parses altWeight to be an int representing grams
-    if (altWeight.find("KG") != -1 or altWeight.find("kg") != -1):
-        altWeight = int(re.sub('[^0-9.]','', altWeight))
-        altWeight = altWeight * 1000
-        print("Package Size: " + str(altWeight) + "g")
-    elif (altWeight.find("G") != -1 or altWeight.find("g") != -1):
-        altWeight = int(re.sub('[^0-9.]','', altWeight))
-        print("Package Size: " + str(altWeight) + "g")
-    else: print("Unknown Package Size")
+    #Parses altWeight to be an float representing grams
+    #Checks that the weight isnt variable eg. 1.4 kg - 2.4 kg
+    if altWeight.find("-") == -1:
+        if (altWeight.find("KG") != -1 or altWeight.find("kg") != -1):
+            altWeight = altWeight.lower()
+            print(altWeight)
+            if (altWeight == "per kg"):
+                altWeight = 1000
+            else:
+                altWeight = float(re.sub('[^0-9.]','', altWeight))
+                altWeight = altWeight * 1000
+            print("Package Size: " + str(altWeight) + "g")
+        elif (altWeight.find("G") != -1 or altWeight.find("g") != -1):
+            altWeight = float(re.sub('[^0-9.]','', altWeight))
+            print("Package Size: " + str(altWeight) + "g")
+        else: print("Unknown Package Size")
+    else: altWeight = "n/a"
 
 
-    #Loop finds and parses protein per 100 grams of product
+    #Loop finds and parses certain nutritional information
     for i in range(len(nutri["Attributes"])):
         if (nutri["Attributes"][i]["Id"] == 878):
             #Removes any non numeric or decimal characters
@@ -72,9 +80,20 @@ def calculations(r_dict, x):
             if (PPS.find(".") == 0):
                 PPS = PPS[1:]
 
+            PPS = float(PPS)
             print("PPS: " + str(PPS))
 
-    #
+        if (nutri["Attributes"][i]["Id"] == 544):
+            servings = nutri["Attributes"][i]["Value"]
+            if servings is not None:
+                servings = servings.lower()
+                if (servings.find("g") == -1):
+                    print("Servings: " + str(servings))
+                else: servings = 0
+
+    #Calculates protein per product if possible
+
+    #Calculates Price Per Gram of Protein
     if cupMeasure == "1KG" or cupMeasure == "1L":
         print("1KG or 1L")
         print("Price per 100g: " + str(cupPrice / 10))
@@ -85,6 +104,8 @@ def calculations(r_dict, x):
         PPGP = cupPrice / pContent
     elif cupMeasure == "1EA" or cupMeasure == "0":
         print("1EA")
+        print(weight)
+        print(price)
         print("Price per 100g: " + str((100 / weight) * price))
         PPGP = ((100 / weight) * price) / pContent 
     else: print("UH OH")
@@ -99,7 +120,7 @@ def addItem(r_dict, x):
 
     with open("itemData.csv", "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["test","test","test","test","test","test","test","test","test","test","test","test","test","test","test"])
+        writer.writerow(["test","test","test","test","test","test","test","test","test","test","test","test","test","test"])
 
 
 #Main logic loop
@@ -130,10 +151,15 @@ for ID in categoryIDs:
         for x in range(itemCount):
             print(r_dict["Bundles"][x]["Name"])
             info = r_dict["Bundles"][x]["Products"][0]["AdditionalAttributes"]["nutritionalinformation"]
+            price = r_dict["Bundles"][x]["Products"][0]["Price"]
             
             #Checks if the selected product contains protein nutritional information
-            if info is None or info.find("Protein") == -1 or info.find('"Protein Quantity Per 100g - Total - NIP\",\"Value\":\"0.0g\"') > 0:
-                print("No protein")
+            if info is None or info.find("Protein Quantity Per 100g") == -1 or info.find('"Protein Quantity Per 100g - Total - NIP\",\"Value\":\"0.0g\"') > 0:
+                print("No protein \n")
+            elif info.find("Protein Quantity Per 100g - Total - NIP\",\"Value\":null") > 0:
+                print("No protein \n")
+            elif price is None:
+                print("No price \n")
             else:
                 print("Protein!")
                 addItem(r_dict, x)
